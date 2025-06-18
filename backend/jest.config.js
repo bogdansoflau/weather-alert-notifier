ac7155e5fc6d24dbacac5976556840d2a000b64f
Bogdan Soflau <bogdansoflau01@gmail.com>
2025-06-19 01:06:43 +0200

    Added GitHub Actions workflow for linting, testing and code coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -7,4 +7,14 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFiles: ["dotenv/config"],
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/server.ts", // omit your entrypoint if you like
+    "!src/app.ts", // omit if you’re not actually testing it
+    "!src/**/index.ts", // ignore barrels
+  ],
+  coverageReporters: ["json", "lcov", "text", "clover"],
 };
